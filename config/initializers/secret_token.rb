a1d9acfc623946def7af012a5aedace9ea3aa3db
Leo Koz <LeonidPr.17@gmail.com>
2011-10-12 23:19:21 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WritingOnTheWall::Application.config.secret_token = 'c14d154481e1261c43f9493ae72d2b05d28502f283d6cbdbc31245e7bc698acf27e79dec4e7f4bf0187e0d8bd5496b5846f856139a19a8e9419953014eaa7f03'
